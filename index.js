cbaadf6df9b0a7f9786f5af9d0dcda9cf87f66d5
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 13:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ class BoardMember {
   }
   
   veto(){return "No, I must disagree"}
+  
   approve(){return "You can do that!"}
+  
   doCharity(){return "I like to help people."}
   
   releasePressStatement(){return "You will see great things from Scuber."}
