bc01a5d518f997dcf4e9817f6ea0d2efded1adeb
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 13:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ class BoardMember {
     this.homeState = homeState;
     this.training  = training;
   }
+  veto(){return "No, I must disagree"}
+  approve(){return "You can do that!"}
+  doCharity(){return "I like to help people."}
+  releasePressStatement(){"You will see great things from Scuber."}
+  sayHi(){}
 }
 
 class Ceo extends BoardMember {
